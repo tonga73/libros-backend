05aa40f6eafa2adf2b576cb016f8200f7b5d4aab
gastoire <gastonmm@gmail.com>
2023-02-09 22:50:06 -0300

    fix: env variables y welcome page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,4 +14,10 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+
+  @Get()
+  async getBooks() {
+    return "MAIN PAGE"
+  }
 }
